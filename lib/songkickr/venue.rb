ca9b1375c78167bbf20b5a0f84a33e65f927cba6
globurg-dev <globurg-dev@googlegroups.com>
2013-07-30 12:12:33 -0400

    Capture all venue data in Venue class

--- lib/songkickr/venue.rb
+++ lib/songkickr/venue.rb
@@ -1,12 +1,25 @@
 module Songkickr
   class Venue
-    attr_accessor :display_name, :id, :lat, :lng
-    
+    attr_accessor :display_name, :id, :lat, :lng,
+      :street, :city, :country, :state, :zip,
+      :website, :phone, :capacity, :description
+
     def initialize(venue_hash)
       @display_name = venue_hash["displayName"]
       @id           = venue_hash["id"]
       @lat          = venue_hash["lat"]
       @lng          = venue_hash["lng"]
+
+      @street       = venue_hash["street"]
+      @city         = venue_hash["city"]["displayName"]
+      @country      = venue_hash["city"]["country"]
+      @state        = venue_hash["city"]["state"]
+      @zip          = venue_hash["city"]["zip"]
+
+      @website      = venue_hash["website"]
+      @phone        = venue_hash["phone"]
+      @capacity     = venue_hash["capacity"]
+      @description  = venue_hash["description"]
     end
   end
 end
